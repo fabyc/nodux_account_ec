733cb7bdd7ba438faa54a7ab2829942d0653417c
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-03-30 11:26:22 -0500

    Separating nodux_party_ec

--- __init__.py
+++ __init__.py
@@ -2,7 +2,6 @@
 #this repository contains the full copyright notices and license terms.#This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
-from .party import *
 from .account import *
 #from .tax import *
 from .product import *
@@ -10,18 +9,13 @@ from .invoice import *
 def register():
     Pool.register(
         #Tax,
-        Company,
-        Address,
-        Configuration,
         CashflowTemplate,
         Cashflow,
         Account,
-        Party,
         AuxiliaryBookStart,
         AuxiliaryPartyStart,
         PrintTrialBalanceDetailedStart,
         PrintTrialBalanceStart,
-        BankAccountNumber,
         OpenCashflowStart,
         PartyWithholdingStart,
         #TaxesByPeriodStart,
