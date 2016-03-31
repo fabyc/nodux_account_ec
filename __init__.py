1737e9d8f651bdd21402951a2bcd90b08d6090c9
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-03-31 15:59:36 -0500

    Division de funcionalidades a nuevo modulo nodux_party_ec

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.#This file is part of Tryton.  The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 from trytond.pool import Pool
+from .party import *
 from .account import *
 #from .tax import *
 from .product import *
@@ -9,6 +10,9 @@ from .invoice import *
 def register():
     Pool.register(
         #Tax,
+        Party,
+        Company,
+        BankAccountNumber,
         CashflowTemplate,
         Cashflow,
         Account,
